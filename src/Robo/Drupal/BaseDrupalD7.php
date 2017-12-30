93e496b77787190af4d0e886f3155e5d1c847ea4
Albert Jankowski <albert@xenomedia.com>
2017-12-29 18:28:23 -0600

    Adds Drupal 8 class.

--- src/Robo/Drupal/BaseDrupalD7.php
+++ src/Robo/Drupal/BaseDrupalD7.php
@@ -3,16 +3,17 @@
 namespace XenoMedia\XenoRobo\Robo\Drupal;
 
 /**
- * Base class for Drupal D7 sites.
+ * Base class for Drupal 7 sites.
  */
 class BaseDrupalD7 extends BaseDrupal {
 
   /**
-   * {@inheritdoc}
+   * Perform set up tasks.
    */
   public function setup() {
+    // If .htaccess file or local settings file does not exit lets create them.
     if (!file_exists($this->getSiteRoot() . '.htaccess') || !file_exists($this->getSiteRoot() . 'sites/default/local.settings.php')) {
-      $this->say("Missing .htaccess or local.setting.php");
+      $this->say("Missing .htaccess or local.settings.php");
       $name = $this->confirm("Missing .htaccess or local.setting.php Copy the default?");
       if ($name) {
         $this->_exec('cp ' . $this->getSiteRoot() . '.htaccess.default ' . $this->getSiteRoot() . '.htaccess');
---
+++ src/Robo/Drupal/BaseDrupalD8.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace XenoMedia\XenoRobo\Robo\Drupal;
+
+/**
+ * Base class for Drupal 8 sites.
+ */
+class BaseDrupalD8 extends BaseDrupal {
+
+  /**
+   * Perform set up tasks.
+   */
+  public function setup() {
+    // If .htaccess file or local settings file does not exit lets create them.
+    if (!file_exists($this->getSiteRoot() . '.htaccess') || !file_exists($this->getSiteRoot() . 'sites/default/settings.local.php')) {
+      $this->say("Missing .htaccess or settings.local.php");
+      $name = $this->confirm("Missing .htaccess or settings.local.php Copy the default?");
+      if ($name) {
+        $this->_exec('cp ' . $this->getSiteRoot() . '.htaccess.default ' . $this->getSiteRoot() . '.htaccess');
+        $this->_exec('cp ' . $this->getSiteRoot() . 'sites/example.settings.local.php ' . $this->$this->getSiteRoot() . 'sites/default/settings.local.php');
+        $this->siteInit = TRUE;
+        $this->start();
+      }
+    }
+    else {
+      $this->siteInit = TRUE;
+    }
+  }
+
+}
